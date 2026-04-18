b2cfb372dc0dbb15afdde6302a462fa591cf2067
RishabhJain-31 <rjrishabhjain31@gmail.com>
2026-04-18 15:46:43 +0530

    "COMMIT"

--- backend-M3M/api_main.py
+++ backend-M3M/api_main.py
@@ -1,3 +1,6 @@
+import os
+import uvicorn
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -27,3 +30,7 @@ app.add_middleware(
 # Mount all version-1 routes under /api/v1
 # Example: POST /api/v1/chat
 app.include_router(api_router, prefix="/api/v1")
+
+# Run the app
+port = int(os.environ.get("PORT", 8000))
+uvicorn.run("api_main:app", host="0.0.0.0", port=port)
